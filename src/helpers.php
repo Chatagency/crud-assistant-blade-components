45fb378e3a30db5fce404f5e074f12ebdd53008d
Victor Sanchez <victor@chatagency.com>
2021-03-29 11:47:02 -0400

    Create new helper to check if variable is template

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,20 @@
 <?php
 
+use Chatagency\CrudAssistant\DataContainer;
 use Chatagency\CrudAssistantBladeComponents\CrudAssistantBladeComponents;
 
 if(!function_exists('CACHelper')) {
     function CACHelper()
     {
-        
         return CrudAssistantBladeComponents::make();
+    }
+}
+
+if(!function_exists('isCACTemplate')) {
+    function isCACTemplate($template)
+    {
+        return is_a($template, DataContainer::class) 
+            && isset($template->template) 
+            && $template->template;
     }
 }
