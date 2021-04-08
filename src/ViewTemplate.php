ac7c6fc2f67c02e5a02d73ce98e443a921efe2c1
Victor Sanchez <victor@chatagency.com>
2021-04-08 15:56:35 -0400

    Add methods to the View Template class

--- src/ViewTemplate.php
+++ src/ViewTemplate.php
@@ -6,11 +6,25 @@ use Chatagency\CrudAssistant\DataContainer;
 
 class ViewTemplate extends DataContainer
 {
+    public function getAttributes()
+    {
+        return $this->attributes ?? [];
+    }
+    
+    public function setAttribute(string $name, $value)
+    {
+        $oldAttributes = $this-> getAttributes();
+        $oldAttributes[$name] = $value;
+        $this->attributes = $oldAttributes;
+
+        return $this;
+    }
+    
     public function setAttributes(array $attributes)
     {
-        $oldAttributes = $this->attributes ?? [];
-
-        $this->attributes = array_merge($oldAttributes, $attributes);
+        foreach($attributes as $name => $value) {
+            $this->setAttribute($name, $value);
+        }
 
         return $this;
     }
