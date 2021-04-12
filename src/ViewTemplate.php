bd72dbf7855609708cc2df4ebe538a5066c26078
Victor Sanchez <victor@chatagency.com>
2021-04-12 09:57:57 -0400

    Add helper methods

--- src/ViewTemplate.php
+++ src/ViewTemplate.php
@@ -6,11 +6,58 @@ use Chatagency\CrudAssistant\DataContainer;
 
 class ViewTemplate extends DataContainer
 {
+    /**
+     * Sets template type.
+     *
+     * @param string $type
+     * 
+     * @return self
+     */
+    public function setType(string $type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Returns template type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    /**
+     * Returns template attributes.
+     *
+     * @return array
+     */
     public function getAttributes()
     {
         return $this->attributes ?? [];
     }
+
+    /**
+     * Returns specific attribute.
+     *
+     * @param string $name
+     */
+    public function getAttribute(string $name)
+    {
+        return $this->getAttributes()[$name] ?? null;
+    }
     
+    /**
+     * Sets specific attribute.
+     *
+     * @param string $name
+     * @param $value
+     * 
+     * @return self
+     */
     public function setAttribute(string $name, $value)
     {
         $oldAttributes = $this-> getAttributes();
@@ -20,6 +67,13 @@ class ViewTemplate extends DataContainer
         return $this;
     }
     
+    /**
+     * Sets attribute array
+     *
+     * @param array $attributes
+     * 
+     * @return self
+     */
     public function setAttributes(array $attributes)
     {
         // $this->attributes = array_merge($this->getAttributes(), $attributes);
