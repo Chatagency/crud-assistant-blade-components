1d6dcf97ae5bf862930e656a9a011a26511f168a
Victor Sanchez <victor@chatagency.com>
2021-04-23 15:17:13 -0400

    Extend from styless

--- resources/views/bootstrap/inputs/option.blade.php
+++ resources/views/bootstrap/inputs/option.blade.php
@@ -1,15 +1,1 @@
-@php
-  $attributes = $input->attributes ?? [];
-@endphp
-<option 
-  @if(!isset($attributes['value']))
-    value="{{ $input->name }}"
-  @endif
-  
-  @include(CACHelper()->partial('attributes'), [
-    'attributes' => $attributes
-  ])
-
-  @if($value == $input->name)
-    selected
-  @endif >{{ $input->label }}</option>
+@include(CACHelper('styleless')->input('option', get_defined_vars()))
